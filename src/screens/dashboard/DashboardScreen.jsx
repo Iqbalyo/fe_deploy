10e1fb8066c6f14c00aa6d0133e87425b992f61a
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-10 11:14:57 +0700

    asa

--- src/screens/dashboard/DashboardScreen.jsx
+++ src/screens/dashboard/DashboardScreen.jsx
@@ -13,25 +13,27 @@ import {
 import AttendancCardsu from "../../components/dashboard/all";
 import TableKehadiran from "../../components/dashboard/TableKehadiran";
 import MainCard from "../../components/dashboard/MainCard/MainCard";
-import UktTable from "../../components/dashboard/informasiUkt/UktTable";
+import UktTable from "../../components/dashboard/UktTable"; // Import UktTable
+import "./dashboard.scss"
+
 const Dashboard = () => {
-const [ semester, setSemester] = useState(null); // State untuk menyimpan semster
-  
+  const [semester, setSemester] = useState(null);
+
   return (
     <div className="content-area">
       <AreaTop />
-      <TopProfil/>
-      <h2>Rekap Presensi</h2> {/* Tampilkan teks sesuai semester */}
-      <TableKehadiran />
-      <UktTable/>
+      <TopProfil />
+      <h2>Rekap Presensi</h2>
+
+      {/* Bungkus dalam div dengan flexbox */}
+      <div className="table-container">
+        <TableKehadiran />
+        <UktTable />
+      </div>
+
       <h2>Informasi Akademik</h2>
-  
-      <MainCard/>
-      <Ipk/>
-      {/*   <AttendanceList/> */}
-      {/* <AreaCharts /> */}
-      {/*<AreaTable />
-      <AreaTable /> */}
+      <MainCard />
+      <Ipk />
     </div>
   );
 };
