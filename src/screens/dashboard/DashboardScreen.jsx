499ad19d36bc786f4ebc8a961f6d4999fc27e0ce
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-10 10:07:37 +0700

    ss

--- src/screens/dashboard/DashboardScreen.jsx
+++ src/screens/dashboard/DashboardScreen.jsx
@@ -13,6 +13,7 @@ import {
 import AttendancCardsu from "../../components/dashboard/all";
 import TableKehadiran from "../../components/dashboard/TableKehadiran";
 import MainCard from "../../components/dashboard/MainCard/MainCard";
+import UktTable from "../../components/dashboard/informasiUkt/UktTable";
 const Dashboard = () => {
 const [ semester, setSemester] = useState(null); // State untuk menyimpan semster
   
@@ -22,6 +23,7 @@ const [ semester, setSemester] = useState(null); // State untuk menyimpan semste
       <TopProfil/>
       <h2>Rekap Presensi</h2> {/* Tampilkan teks sesuai semester */}
       <TableKehadiran />
+      <UktTable/>
       <h2>Informasi Akademik</h2>
   
       <MainCard/>
