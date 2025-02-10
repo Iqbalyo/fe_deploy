945fd8d45cf42263e13ffe687aa6a927a2ea790b
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-10 10:30:35 +0700

    a

--- src/components/dashboard/TableKehadiran/TableKehadiran.jsx
+++ src/components/dashboard/TableKehadiran/TableKehadiran.jsx
@@ -1,20 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAbsensiById } from '../../../store/action/userData.action';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import ListItemButton from '@mui/material/ListItemButton';
-import styles from './TableKehadiran.module.css';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAbsensiById } from "../../../store/action/userData.action";
+import "./TableKehadiran.scss"; // Menggunakan SCSS
 
 const TableKehadiran = () => {
-  const nim = localStorage.getItem('nim');
+  const nim = localStorage.getItem("nim");
   const dispatch = useDispatch();
-  const { absen, currentSemester, currentPeriode, lastSemester } = useSelector((state) => state.data);
-  
+  const { absen } = useSelector((state) => state.data);
+
   useEffect(() => {
     if (nim) {
       dispatch(getAbsensiById(nim));
@@ -22,40 +15,25 @@ const TableKehadiran = () => {
   }, [dispatch, nim]);
 
   return (
-    <Paper className={styles.container}>
-      {currentSemester && currentPeriode && lastSemester && (
-        <div className={styles.semesterInfo}>
-          <Typography variant="h6">
-            Semester: {lastSemester} ({currentSemester}) - Periode: {currentPeriode}
-          </Typography>
-        </div>
-      )}
-      <List>
+    <div className="kehadiran-container">
+      <h2 className="kehadiran-title">Rekap Kehadiran</h2>
+      <div className="kehadiran-list">
         {absen && absen.length > 0 ? (
           absen.map((row, index) => (
-            <ListItem key={index} alignItems="flex-start">
-              <ListItemButton component={Link} to={`/informasi-kehadiran/${row.matakuliah_nama}`}>
-                <ListItemText
-                  primary={row.matakuliah_nama}
-                  secondary={
-                    <>
-                      
-                      <Typography variant="body2">Hadir: {row.hadir}</Typography>
-                      <Typography variant="body2">Izin: {row.izin}</Typography>
-                      <Typography variant="body2">Tanpa Keterangan: {row.tanpaKeterangan}</Typography>
-                    </>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
+            <div key={index} className="kehadiran-item">
+              <span className="mata-kuliah">{row.matakuliah_nama}</span>
+              <div className="info-kehadiran">
+                <span className="hadir">Hadir: {row.hadir}</span>
+                <span className="izin">Izin: {row.izin}</span>
+                <span className="alpha">Tanpa Keterangan: {row.tanpaKeterangan}</span>
+              </div>
+            </div>
           ))
         ) : (
-          <Typography variant="body1" align="center">
-            Tidak ada data absensi
-          </Typography>
+          <p className="no-data">Tidak ada data absensi</p>
         )}
-      </List>
-    </Paper>
+      </div>
+    </div>
   );
 };
 
