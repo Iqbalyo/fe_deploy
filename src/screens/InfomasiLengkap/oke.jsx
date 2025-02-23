d290e221f678c89766f7a93382709143d3cbe46a
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-23 11:09:21 +0700

    sasa

--- src/screens/InfomasiLengkap/oke.jsx
+++ src/screens/InfomasiLengkap/oke.jsx
@@ -60,6 +60,7 @@ const Oke = () => {
             <TableCell>Nama Mata Kuliah</TableCell>
             <TableCell>Pertemuan Ke</TableCell>
             <TableCell>Status</TableCell>
+            <TableCell>Dosen</TableCell>
             <TableCell>Tanggal Kuliah</TableCell>
             <TableCell>Waktu</TableCell>
           </TableRow>
