3213ec5f10c0bb2883cb72c98ccd950db4a8828c
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-26 23:31:52 +0700

    aqq

--- src/components/dashboard/Profilmahasiswa/TopProfil.jsx
+++ src/components/dashboard/Profilmahasiswa/TopProfil.jsx
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Icon } from "semantic-ui-react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { FaBars } from "react-icons/fa";
 
 const TopProfil = () => {
-  const [setMahasiswa] = useState(null); // State untuk data mahasiswa
-  const nim = localStorage.getItem("nim"); // Ambil NIM dari localStorage
-  const nama = localStorage.getItem("nama"); // Ambil Nama dari localStorage
+  const [setMahasiswa] = useState(null);
+  const nim = localStorage.getItem("nim");
+  const nama = localStorage.getItem("nama");
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 762);
 
   useEffect(() => {
     if (nim) {
-      // Fetch data mahasiswa untuk mendapatkan profil
       fetch(`https://be-deploy-sage.vercel.app/api/mahasiswa/${nim}`)
         .then((response) => response.json())
         .then((data) => setMahasiswa(data))
         .catch((error) => console.error("Error fetching mahasiswa data:", error));
     }
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 762);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [nim]);
 
   return (
@@ -24,19 +32,21 @@ const TopProfil = () => {
       alignItems="center"
       justifyContent="space-between"
       padding={2}
-      sx={{ backgroundColor: "#ffffff", borderRadius: "8px",
-        width: "100%" ,
-        marginLeft: "auto" // Menggeser ke kanan
-       }}
+      sx={{ backgroundColor: "#ffffff", borderRadius: "8px", width: "100%" }}
     >
-      <Typography variant="h5" sx={{ fontWeight: "bold", marginRight: "auto" }}>
-        Universitas Dinamika Bangsa
-      </Typography>
+      <Box display="flex" alignItems="center">
+        {isMobile && (
+          <FaBars size={24} style={{ marginRight: 10, cursor: "pointer" }} />
+        )}
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          {isMobile ? "UNAMA" : "Universitas Dinamika Bangsa"}
+        </Typography>
+      </Box>
       <Box display="flex" alignItems="center">
         <Typography variant="body1" sx={{ fontWeight: "bold", marginRight: 1 }}>
           {nama || "Nama Tidak Ditemukan"}
         </Typography>
-        <Icon name="user circle" size="big"  style={{ color: "#ff207d", marginRight: "16px" }} /> {/* Ikon user */}
+        <Icon name="user circle" size="big" style={{ color: "#ff207d", marginRight: "16px" }} />
       </Box>
     </Box>
   );
