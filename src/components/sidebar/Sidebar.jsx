331203b7d0490a7c381330c7795b76fc4d24e6cc
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-26 15:28:54 +0700

    a

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,44 +1,16 @@
-import { useContext, useEffect, useRef } from "react";
-import { useLocation, Link } from "react-router-dom"; //! Tambahkan useLocation,Tambahkan useLocation dari react-router-dom dan gunakan URL saat ini untuk menentukan menu mana yang aktif.
-import { ThemeContext } from "../../context/ThemeContext";
-import { SidebarContext } from "../../context/SidebarContext";
-import {
-  MdOutlineAttachMoney,
-  MdOutlineClose,
-  MdOutlineGridView,
-  MdOutlinePeople,
-  MdSchool,
-} from "react-icons/md";
+import { useContext } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { SidebarContext } from "../../context/SidebarContext"; // Import SidebarContext
+import { MdOutlineAttachMoney, MdOutlineClose, MdOutlineGridView, MdOutlinePeople, MdSchool } from "react-icons/md";
 import Logounama from "../../assets/images/unama.png";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useContext(SidebarContext); // Ambil state & fungsi dari context
-  const location = useLocation(); // Gunakan useLocation
-
-  // Fungsi untuk menutup sidebar jika diklik di luar area
-  const handleClickOutside = (event) => {
-    if (
-      navbarRef.current &&
-      !navbarRef.current.contains(event.target) &&
-      !event.target.classList.contains("sidebar-toggle-btn")
-    ) {
-      closeSidebar();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  const location = useLocation();
 
   return (
-    <nav
-      className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
-      ref={navbarRef}
-    >
+    <nav className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}>
       <div className="sidebar-top">
         <div className="sidebar-brand">
           <img
@@ -62,69 +34,19 @@ const Sidebar = () => {
         <div className="sidebar-menu">
           <ul className="menu-list">
             <li className="menu-item">
-              <Link
-                to="/"
-                className={`menu-link ${
-                  location.pathname === "/" ? "active" : ""
-                }`}
-                 onClick={() => closeSidebar()} 
-              >
+              <Link to="/" className={`menu-link ${location.pathname === "/" ? "active" : ""}`} onClick={closeSidebar}>
                 <span className="menu-link-icon">
                   <MdOutlineGridView size={18} />
                 </span>
-                <span className="menu-link-text">Dasboard</span>
+                <span className="menu-link-text">Dashboard</span>
               </Link>
             </li>
             <li className="menu-item">
-              <Link
-                to="/informasikehadiran"
-                className={`menu-link ${
-                  location.pathname === "/informasikehadiran" ? "active" : ""
-                }`}
-              >
+              <Link to="/informasikehadiran" className={`menu-link ${location.pathname === "/informasikehadiran" ? "active" : ""}`} onClick={closeSidebar}>
                 <span className="menu-link-icon">
                   <MdOutlinePeople size={20} />
                 </span>
                 <span className="menu-link-text">Informasi Kehadiran</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link
-                to="/ipk"
-                className={`menu-link ${
-                  location.pathname === "/ipk" ? "active" : ""
-                }`}
-              >
-                <span className="menu-link-icon">
-                  <MdSchool size={18} />
-                </span>
-                <span className="menu-link-text">IPK</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link
-                to="/informasiukt"
-                className={`menu-link ${
-                  location.pathname === "/informasiukt" ? "active" : ""
-                }`}
-              >
-                <span className="menu-link-icon">
-                  <MdOutlineAttachMoney size={20} />
-                </span>
-                <span className="menu-link-text">Biaya Kuliah</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link
-                to="/logout"
-                className={`menu-link ${
-                  location.pathname === "/logout" ? "active" : ""
-                }`}
-              >
-                {/* <span className="menu-link-icon">
-                  <MdOutlineClose size={20} />
-                </span>
-                <span className="menu-link-text">Logout</span> */}
               </Link>
             </li>
           </ul>
