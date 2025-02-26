d3dde8ccaffc80ec56ea432c1e1a14cd217e73dc
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-26 15:42:09 +0700

    gg

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk"; // Tidak perlu destructuring {}
+import * as thunk from "redux-thunk";
+
+// import thunk from "redux-thunk"; // Tidak perlu destructuring {}
 import reducer from "./reducer/index";
 
 const store = createStore(reducer, compose(applyMiddleware(thunk)));
