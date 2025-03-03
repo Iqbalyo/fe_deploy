0a018a1b6cfc52f502a6353c7a41ca2b147a74cb
iqbalyo <iqbalmustakim734@gmail.com>
2025-03-04 06:51:07 +0700

    asa

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-import { useContext, useEffect, useRef, useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useContext, useEffect, useRef } from "react";
+import { useLocation, Link } from "react-router-dom"; //! Tambahkan useLocation,Tambahkan useLocation dari react-router-dom dan gunakan URL saat ini untuk menentukan menu mana yang aktif.
 import { ThemeContext } from "../../context/ThemeContext";
 import { SidebarContext } from "../../context/SidebarContext";
 import {
@@ -16,23 +16,11 @@ const Sidebar = () => {
   const { theme } = useContext(ThemeContext);
   const { isSidebarOpen, closeSidebar } = useContext(SidebarContext);
   const navbarRef = useRef(null);
-  const location = useLocation();
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const location = useLocation(); // Gunakan useLocation
 
-  // Fungsi untuk update state saat ukuran layar berubah
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  // Fungsi untuk menutup sidebar jika diklik di luar area pada mobile
+  // Fungsi untuk menutup sidebar jika diklik di luar area
   const handleClickOutside = (event) => {
     if (
-      isMobile &&
       navbarRef.current &&
       !navbarRef.current.contains(event.target) &&
       event.target.className !== "sidebar-open-btn"
@@ -42,19 +30,16 @@ const Sidebar = () => {
   };
 
   useEffect(() => {
-    if (isMobile) {
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }
-  }, [isMobile]);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav
-      className={`sidebar ${isSidebarOpen || !isMobile ? "sidebar-show" : ""}`}
-      ref={navbarRef}
-    >
+      className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
+      ref={navbarRef}>
       <div className="sidebar-top">
         <div className="sidebar-brand">
           <img
@@ -70,59 +55,80 @@ const Sidebar = () => {
           />
           <span className="sidebar-brand-text">UNAMA</span>
         </div>
-        {isMobile && (
-          <button className="sidebar-close-btn" onClick={closeSidebar}>
-            <MdOutlineClose size={24} />
-          </button>
-        )}
+        <button className="sidebar-close-btn" onClick={closeSidebar}>
+          <MdOutlineClose size={24} />
+        </button>
       </div>
       <div className="sidebar-body">
-        <ul className="menu-list">
-          <li className="menu-item">
-            <Link
-              to="/"
-              className={`menu-link ${location.pathname === "/" ? "active" : ""}`}
-            >
-              <span className="menu-link-icon">
-                <MdOutlineGridView size={18} />
-              </span>
-              <span className="menu-link-text">Dashboard</span>
-            </Link>
-          </li>
-          <li className="menu-item">
-            <Link
-              to="/informasikehadiran"
-              className={`menu-link ${location.pathname === "/informasikehadiran" ? "active" : ""}`}
-            >
-              <span className="menu-link-icon">
-                <MdOutlinePeople size={20} />
-              </span>
-              <span className="menu-link-text">Informasi Kehadiran</span>
-            </Link>
-          </li>
-          <li className="menu-item">
-            <Link
-              to="/ipk"
-              className={`menu-link ${location.pathname === "/ipk" ? "active" : ""}`}
-            >
-              <span className="menu-link-icon">
-                <MdSchool size={18} />
-              </span>
-              <span className="menu-link-text">IPK</span>
-            </Link>
-          </li>
-          <li className="menu-item">
-            <Link
-              to="/informasiukt"
-              className={`menu-link ${location.pathname === "/informasiukt" ? "active" : ""}`}
-            >
-              <span className="menu-link-icon">
-                <MdOutlineAttachMoney size={20} />
-              </span>
-              <span className="menu-link-text">Biaya Kuliah</span>
-            </Link>
-          </li>
-        </ul>
+        <div className="sidebar-menu">
+          <ul className="menu-list">
+            <li className="menu-item">
+              <Link
+                to="/"
+                className={`menu-link ${
+                  location.pathname === "/" ? "active" : ""
+                }`}
+              >
+                <span className="menu-link-icon">
+                  <MdOutlineGridView size={18} />
+                </span>
+                <span className="menu-link-text">Dasboard</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link
+                to="/informasikehadiran"
+                className={`menu-link ${
+                  location.pathname === "/informasikehadiran" ? "active" : ""
+                }`}
+              >
+                <span className="menu-link-icon">
+                  <MdOutlinePeople size={20} />
+                </span>
+                <span className="menu-link-text">Informasi Kehadiran</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link
+                to="/ipk"
+                className={`menu-link ${
+                  location.pathname === "/ipk" ? "active" : ""
+                }`}
+              >
+                <span className="menu-link-icon">
+                  <MdSchool size={18} />
+                </span>
+                <span className="menu-link-text">IPK</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link
+                to="/informasiukt"
+                className={`menu-link ${
+                  location.pathname === "/informasiukt" ? "active" : ""
+                }`}
+              >
+                <span className="menu-link-icon">
+                  <MdOutlineAttachMoney size={20} />
+                </span>
+                <span className="menu-link-text">Biaya Kuliah</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link
+                to="/logout"
+                className={`menu-link ${
+                  location.pathname === "/logout" ? "active" : ""
+                }`}
+              >
+                {/* <span className="menu-link-icon">
+                  <MdOutlineClose size={20} />
+                </span>
+                <span className="menu-link-text">Logout</span> */}
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
